a8ca155d1eccac86933c098c5a2ef736025b4b83
Prince <xiuuscv@gmail.com>
2022-05-18 08:31:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.111/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
 sed -i 's/ucidef_set_interfaces_lan_wan 'eth1' 'eth0'/ucidef_set_interfaces_lan_wan 'eth0' 'eth1'/g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
