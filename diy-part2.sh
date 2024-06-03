4f797bf5740c8f2f7d0c0ceae36752902542f947
Prince <xiuuscv@gmail.com>
2024-06-03 14:59:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,6 @@
 sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 # sed -i 's/ucidef_set_interfaces_lan_wan 'eth1' 'eth0'/ucidef_set_interfaces_lan_wan 'eth0' 'eth1'/g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
+# Modify hostname
+sed -i 's/OpenWrt/OverPrinceWRT/g' package/base-files/files/bin/config_generate
